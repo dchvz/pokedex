18b34e8e5266a5d96ecf9c023c8ff46402324b90
Diego Chavez <Diego.Chavez@coxautoinc.com>
2023-06-13 23:13:15 -0400

    Make card component pressable

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,17 @@
-import {StyleSheet, View, ViewStyle} from 'react-native';
+import {StyleSheet, Pressable, ViewStyle} from 'react-native';
 import React, {PropsWithChildren} from 'react';
 
 type Props = {
   viewStyle?: ViewStyle[];
+  onPress?: () => void;
 };
 
-const Card = ({children, viewStyle}: PropsWithChildren<Props>) => {
-  return <View style={[viewStyle, styles.container]}>{children}</View>;
+const Card = ({children, viewStyle, onPress}: PropsWithChildren<Props>) => {
+  return (
+    <Pressable onPress={onPress} style={[viewStyle, styles.container]}>
+      {children}
+    </Pressable>
+  );
 };
 
 interface IStyles {
