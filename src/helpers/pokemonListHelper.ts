79e0d1d59e755a49f0c72960505f1254f695dccf
Diego Chavez <Diego.Chavez@coxautoinc.com>
2023-06-14 23:47:07 -0400

    Update helper method to return normal/lighter color for a pokemon type

--- src/helpers/pokemonListHelper.ts
+++ src/helpers/pokemonListHelper.ts
@@ -1,8 +1,11 @@
-import {COLOR_BY_TYPE} from '../constants/types';
+import {COLOR_BY_TYPE, LIGHT_COLOR_BY_TYPE} from '../constants/types';
 import {PokemonType} from '../types/types';
 
-export const getColorByType = (type: PokemonType): string => {
-  return COLOR_BY_TYPE[type];
+export const getColorByType = (
+  type: PokemonType,
+  useLighterColor: boolean = false,
+): string => {
+  return useLighterColor ? LIGHT_COLOR_BY_TYPE[type] : COLOR_BY_TYPE[type];
 };
 
 export const upperCaseFirstLetter = (name: string): string => {
